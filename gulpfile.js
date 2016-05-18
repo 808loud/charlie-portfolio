67fbab6fd91d4d4bbbffc900491940e91daea7ef
David Monson <david@808loud.com>
2016-05-18 15:27:01 -0500

    updated gulp flow with os notifications and error handling

--- gulpfile.js
+++ gulpfile.js
@@ -1,93 +1,89 @@
 'use strict';
 
-var gulp =           require('gulp'),
-    sequence =       require('run-sequence'), // won't be necessary if gulp -v > 4.0
-    //nunjucksRender = require('gulp-nunjucks-render'),
-    nunjucks =       require('gulp-nunjucks-html'),
-    sass =           require('gulp-sass'),
-    //server =         require('gulp-server-livereload'),
-    webserver =      require('gulp-webserver'),
-    maps =           require('gulp-sourcemaps'),
-    concat =         require('gulp-concat');
+var gulp =         require('gulp');
+var gutil =         require('gulp-util');
+var data =         require('gulp-data');
+var concat =       require('gulp-concat');
+var nunjucks =     require('gulp-nunjucks-html');
+var sass =         require('gulp-sass');
+var sourcemaps =   require('gulp-sourcemaps');
+var notify =       require('gulp-notify');
+var browserSync =  require('browser-sync').create();
 
+var reload = browserSync.reload;
+
+// https://github.com/mikaelbr/gulp-notify/issues/81
 function catchError (error) {
-    console.log(error.toString());
+
+    var lineNumber = (error.lineNumber) ? 'LINE ' + error.lineNumber + ' -- ' : '';
+    
+    notify({
+        title: 'Gulp Task [' + error.plugin + '] Error',
+        message: lineNumber
+    }).write(error);
+
+    // Pretty error reporting
+    var report = '';
+    var chalk = gutil.colors.white.bgRed;
+
+    report += chalk('TASK:') + ' [' + error.plugin + ']\n';
+    report += chalk('PROB:') + ' ' + error.message + '\n';
+    if (error.lineNumber) { report += chalk('LINE:') + ' ' + error.lineNumber + '\n'; }
+    if (error.fileName)   { report += chalk('FILE:') + ' ' + error.fileName + '\n'; }
+    console.error(report);
+
     this.emit('end');
 }
+
+gulp.task('serve', ['nunjucks', 'sass', 'scripts'], function() { 
+    browserSync.init({ 
+        server: {
+            baseDir: "./public",
+            index: "index.html"
+        },
+        port: 5555
+    }); 
+
+    gulp.watch(['./app/templates/**/*.+(html|nunjucks|njk)', 
+                './app/pages/**/*.+(html|nunjucks|njk)'], 
+                ['nunjucks']); 
+    gulp.watch('./app/scss/**/*.scss', ['sass']); 
+    gulp.watch('./app/js/**/*.js', ['scripts']);
+    gulp.watch("./public/**/*.*").on('change', reload); 
+});
 
 // tasks
 gulp.task('nunjucks', function() {
   // Gets .html and .nunjucks files in pages
-  return gulp.src('app/pages/**/*.+(html|nunjucks|njk)')
+  return gulp.src('./app/pages/**/*.+(html|nunjucks|njk)')
   // Renders template with nunjucks
   .pipe(nunjucks({
-      searchPaths: ['app/templates'],
+      searchPaths: ['./app/templates'],
       autoescape: ['true'],
       ext: ['.html']
     }))
   .on('error', catchError)
   // output files in public folder
-  .pipe(gulp.dest('public'))
+  .pipe(gulp.dest('./public'))
 });
 
 gulp.task('sass', function () {
     return gulp.src('./app/scss/**/*.+(sass|scss)')
-        .pipe(maps.init())
-        .pipe(sass())
-        .pipe(maps.write())
+        .pipe(sourcemaps.init())
+        .pipe(sass({ 
+            errLogToConsole: true 
+        }))
+        .on('error', catchError)
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('scripts', function() {
     return gulp.src('./app/js/**/*.js')
-        .pipe(maps.init())
+        .pipe(sourcemaps.init())
         .pipe(concat('scripts.js'))
-        .pipe(maps.write())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('./public/js/'))
 });
 
-/*
-gulp.task('webserver', function() {
-    return gulp.src('./')
-        .pipe(server({
-            livereload: true,
-            directoryListing: true,
-            defaultFile: 'public/index.html',
-            open: true,
-            port: 5555
-        }));
-});
-*/
-gulp.task('webserver', function() {
-  gulp.src('./')
-      .pipe(webserver({
-          livereload: true,
-          directoryListing: true,
-          open: 'public/index.html'
-      }));
-})
-
-// watchers
-gulp.task('nunjucks:watch', function () {
-  gulp.watch(['./app/templates/**/*.+(html|nunjucks|njk)', './app/pages/**/*.+(html|nunjucks|njk)'], ['nunjucks']);
-});
-
-gulp.task('sass:watch', function () {
-  gulp.watch('./app/scss/**/*.scss', ['sass']);
-});
-
-gulp.task('scripts:watch', function () {
-  gulp.watch('./app/js/**/*.js', ['scripts']);
-});
-
-gulp.task('default', function(done) {
-    sequence(
-        'nunjucks',
-        'sass',
-        'scripts',
-        'nunjucks:watch',
-        'sass:watch',
-        'scripts:watch',
-        'webserver',
-    done);
-});
+gulp.task('default', ['serve']);
