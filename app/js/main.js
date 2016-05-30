f5fe1d087587fef4471b0d864535e610e2cf8098
David Monson <david@808loud.com>
2016-05-29 22:13:12 -0500

    style tweaks

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,5 @@
 
-$('.carousel').carousel();
-
-console.log("hi");
+$('.carousel').carousel({ pause: "false" });
 
 $(document).ready(
 	function carouselNormalization() {
@@ -11,20 +9,16 @@ $(document).ready(
 			tallest; //create variable to make note of the tallest slide
 
 		if (items.length) {
-			console.log("has length");
 			function normalizeHeights() {
-					console.log("normalize");
 					items.each(function() { //add heights to array
 						heights.push($(this).height()); 
 					});
 					tallest = Math.max.apply(null, heights); //cache largest value
 					items.each(function() {
 						$(this).css('min-height',tallest + 'px');
-						console.log("set heights");
 					});
 			};
 			normalizeHeights();
-			console.log("ran the function");
 
 			$(window).on('resize orientationchange', function () {
 				tallest = 0, heights.length = 0; //reset vars
@@ -32,7 +26,6 @@ $(document).ready(
 					$(this).css('min-height','0'); //reset min-height
 				}); 
 				normalizeHeights(); //run it again 
-				console.log("run on resize");
 			});
 		}
 	}
